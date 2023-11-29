377062b06e4c26460647463bfdec16bb96633ce1
Nezzhub <152405568+Nezzhub@users.noreply.github.com>
2023-11-29 16:50:27 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Add JavaScript to toggle the visibility of the dropdown
+document.addEventListener('DOMContentLoaded', function () {
+  const dropdown = document.querySelector('.dropdown');
+
+  dropdown.addEventListener('mouseenter', function () {
+    const dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = 'block';
+  });
+
+  dropdown.addEventListener('mouseleave', function () {
+    const dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = 'none';
+  });
+});
